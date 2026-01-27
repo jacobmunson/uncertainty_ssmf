42e400abdd2ae996cdb9722d2744986cf231b6af
Jacob Munson <43017899+jacobmunson@users.noreply.github.com>
2026-01-26 20:50:53 -0700

    Add files via upload

---
+++ activate_uncertainty_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source /etc/profile
+
+# Purge all loaded modules to avoid conflicts
+module purge
+
+# Load required modules
+module load GCCcore/13.2.0
+module load GCC/13.2.0
+module load OpenMPI/4.1.6-GCC-13.2.0
+
+# Source your bash config (for conda)
+source ~/.bashrc
+
+# Activate the desired conda environment
+conda activate UncertaintySSMF_env
